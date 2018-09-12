29409d552cab6aa825ffdac07ec87acd0fa7f453
maniyarasan16 <manismvss@gmail.com>
2018-09-12 14:06:00 -0700

    added sample

---
+++ src/org/repos/NewRepos.java
@@ -0,0 +1,14 @@
+package org.repos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewRepos {
+	public static void main(String[] args){
+		List ex=new ArrayList();
+		ex.add(10);
+		System.out.println(ex);
+		
+	}
+
+}
